95a49c72f9b1aadf136b984deb07e74857e60c40
unknown <abubakerdhakko>
2023-06-13 23:11:35 +0500

    aa

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    res.send('Hello, Wozzzrld hi!');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
