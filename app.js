9a761661dd99f056c2eea18dd15beb44b3ca0057
abu baker Dhakko <36266960+abubakerdhakko@users.noreply.github.com>
2023-06-13 14:59:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-// Import the Express module
 const express = require('express');
-
-// Create an Express application
 const app = express();
+const port = process.env.PORT || 3000;
 
-// Define a route to handle requests to the root URL
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+  res.send('Hello, World!');
 });
 
-// Start the server
-app.listen(8080, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
